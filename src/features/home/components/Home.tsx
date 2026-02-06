2dd63f872380a84033ecba2e05fd45a0c5cc48cf
Ali Armaghan <armaghanbinali@gmail.com>
2026-02-06 18:55:09 +0500

    feat: add SearchIcon and Menuicon components and update Home component to display a new search and menu UI.

--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -11,7 +11,8 @@ import EllipseWhite from '@/components/icons/ellipswhite'
 import Man from '@/components/icons/man'
 import Star from '@/components/icons/star'
 import Slideicon from '@/components/icons/slideicon'
-
+import Menuicon from '@/components/icons/menuicon'
+import SearchIcon from '@/components/icons/searchicon'
 
 const Home = () => {
     return (
@@ -104,8 +105,19 @@ const Home = () => {
                             <EllipseWhite />
                         </div>
                         {/* search menu div */}
-                        <div className='absolute bg-amber-300 w-[311px] h-[53px] right-[90px] top-[79px]'>
-                            <input type="text" />
+                        <div className='absolute z-50 w-[361px] h-[53px] right-[90px] top-[79px] flex items-center justify-evenly'>
+                            <div className='flex items-center gap-2 w-[146px] h-[53px] px-4 border-2 border-transparent rounded-full bg-[linear-gradient(#334B35,#334B35),linear-gradient(150deg,rgba(255,255,255,0.75),transparent,rgba(255,255,255,0.75))] bg-origin-border [background-clip:padding-box,border-box] relative'>
+                                <SearchIcon size={50} />
+                                <input type="text" placeholder="" className='bg-transparent border-none outline-none text-white text-[16px] w-full relative z-10' />
+                            </div>
+                            <p className='text-white text-[20px]'
+                                style={{
+                                    fontFamily: Theme.fonts.default,
+                                    fontWeight: Theme.fonts.weight.medium,
+                                }}
+                            >Menu</p>
+                            <Menuicon />
+
                         </div>
                         {/* <div className='bg-amber-300 w-[415px] h-[267px] top-[302px] right-[42px] absolute flex justify-end items-end'> */}
                         <div className='  w-[415px] h-[267px] top-[200px] right-[42px] absolute flex justify-end items-end z-0'>
@@ -113,7 +125,7 @@ const Home = () => {
                                 <Man />
                             </div>
                             {/* bg div */}
-                            <div className='absolute h-[112px] w-[367px] z-10 bottom-0 left-0 border-2 border-transparent rounded-[15px] bg-[linear-gradient(#113014,#113014),linear-gradient(150deg,rgba(255,255,255,0.75),transparent,rgba(255,255,255,0.75))] bg-origin-border [background-clip:padding-box,border-box]'>
+                            <div className='absolute h-[112px] w-[367px] z-10 bottom-0 left-0 border-2 border-transparent rounded-[15px] bg-[linear-gradient(#1B371F,#1B371F),linear-gradient(150deg,rgba(255,255,255,0.75),transparent,rgba(255,255,255,0.75))] bg-origin-border [background-clip:padding-box,border-box]'>
                                 <div className='ml-[34px] mt-[-15px] w-[78px] h-[28px] bg-[#066400] rounded-[4px] text-center text-[14px] flex justify-center items-center'>
                                     <p
                                         style={{
